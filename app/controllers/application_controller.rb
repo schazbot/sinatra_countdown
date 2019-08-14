d6579daf690371b12a467d985ca463b707f61071
schazbot <charlotte.pearce1984@gmail.com>
2019-08-14 16:54:09 +0100

    form sending params but not rendering view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,55 @@
-require './config/environment'
+require "./config/environment"
 
 class ApplicationController < Sinatra::Base
-
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
-  get "/" do
-    erb :new
+  # GET: /boards
+  get "/boards" do
+    erb :"/index.html"
   end
 
+  # GET: /boards/new
+  get "/new" do
+    erb :"/new.html"
+  end
+
+  post "/create" do
+    puts params
+    
+    @letter_1 = params[:letter_1]
+    @letter_2 = params[:letter_2]
+    @letter_3 = params[:letter_3]
+    @letter_4 = params[:letter_4]
+    @letter_5 = params[:letter_5]
+    @letter_6 = params[:letter_6]
+    @letter_7 = params[:letter_7]
+
+    redirect '/show'
+  end
+
+
+
+  # GET: /boards/5
+  get "/:id" do
+    erb :"/show.html"
+  end
+
+  # GET: /boards/5/edit
+  get "/:id/edit" do
+    erb :"/edit.html"
+  end
+
+  # PATCH: /boards/5
+  patch "/:id" do
+    redirect "/boards/:id"
+  end
+
+  # DELETE: /boards/5/delete
+  delete "/:id/delete" do
+    redirect "/boards"
+  end
+ 
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,22 @@
-class Board < ActiveRecord::Base
+class Board 
+  attr_accessor :letter_1, :letter_2, :letter_3, :letter_4, :letter_5, :letter_6, :letter_7
 
-    
+  @@all = []
 
+  def initialize(letter_1, letter_2, letter_3, letter_4, letter_5, letter_6, letter_7)
+    @letter_1 = letter_1
+    @letter_2 = letter_2
+    @letter_3 = letter_3
+    @letter_4 = letter_4
+    @letter_5 = letter_5
+    @letter_6 = letter_6
+    @letter_7 = letter_7
+
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
 end
